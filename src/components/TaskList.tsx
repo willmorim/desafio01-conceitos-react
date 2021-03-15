83e9ae40eccdad11736e83f7734e4d21bc1e0b78
willmorim <pdbawilliam@gmail.com>
2021-03-15 01:00:50 -0300

    Complete methods for the challenge

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,34 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if(!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const taskChecked = tasks.map(task => task.id === id ? { 
+      ...task, 
+      isComplete: !task.isComplete 
+    } : task);
+
+    setTasks(taskChecked);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const removeTask = tasks.filter(task => task.id !== id);
+
+    setTasks(removeTask);
   }
 
   return (
